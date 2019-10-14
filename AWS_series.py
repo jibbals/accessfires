9e00c7f27915e09efd248cd1995536377c02a86b
Jesse Greenslade <bboyjesse@gmail.com>
2019-10-14 16:49:12 +1100

    AWS correlations added to script

--- AWS_series.py
+++ AWS_series.py
@@ -7,6 +7,7 @@ Created on Mon Sep 30 11:47:15 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import patches
 import pandas
 import iris
 from copy import deepcopy
@@ -247,30 +248,75 @@ def compare_site_to_model(AWS='wagerup',
     df_agg = df.resample('30Min').mean()
     df_time_series(df_agg, subplots=subplots)
     fio.save_fig(model_run=model_run, script_name=_sn_, 
-                 pname='%s_%s_aggregate%s'%(AWS,model_run,['','_UTC'][UTC]), plt=plt) 
+                 pname='%s_%s_aggregate%s'%(AWS,model_run,['','_UTC'][UTC]), plt=plt)
     
     ## Now we look at statistics
     # if any columns are NAN, drop whole row
     df_agg = df_agg.dropna(how='any') 
-    # now get correlation,bias between certaint column pairs:
-    comparipairs = [['Dta10','wind_direction10'],['S10','s10'],['QFE','air_pressure10'],['RH','relative_humidity10'],['T','air_temperature1']]
     
-    for pair in comparipairs:
-        # straight up correlation
+    # diurnal half hourly cycle
+    diurnal = df.resample('30Min').mean() # 30 min averages
+    diurnal = diurnal.groupby([diurnal.index.hour, diurnal.index.minute]).mean() # combine multiple days
+    
+    # now get correlation,bias between certaint column pairs:
+    titles = ['Wind dir', 'RH','T','Wind speed','Pressure']
+    pairs = [['Dta10','wind_direction10'],['RH','relative_humidity10'],['T','air_temperature1'],['S10','s10'],['QFE','air_pressure10']]
+    
+    fig, axes = plt.subplots(nrows=5, sharex=True, figsize=[7,12])
+    
+    # for each pair, show them detrended and the correlation before/after detrending
+    for j, (pair, title) in enumerate(zip(pairs,titles)):
+        plt.sca(axes[j])
+        
         corr = df_agg[pair].corr()
-        print("DEBUG: correlation:",corr)
         
-        # remove diurnal trend (measured) from both measurement and model time series:
-        diurnal = df[pair[0]]
+        # detrend the pair:
+        # model data being detrended with measurements (longer time series)
+        dpair = [pair[0],pair[0]] # just measured column twice
+        hours,minutes = df_agg.index.hour, df_agg.index.minute
+        diff = []
+        for jj in range(len(df_agg)):
+            H,M = hours[jj], minutes[jj]
+            diff.append( df_agg[pair].values[jj] - diurnal[dpair].loc[(H,M)].values)
+        # make dataframe using differences, column names, and set index to match df_agg
+        detrended = pandas.DataFrame(diff, columns=pair)
+        detrended.index = df_agg.index
+        # correlation
+        detrended_corr = detrended.corr()
         
-        # annotate onto correct axis
-        ax.annotate('axes fraction',
-                    xy=(3, 1), xycoords='data',
-                    xytext=(0.8, 0.95), textcoords='axes fraction',
-                    arrowprops=dict(facecolor='black', shrink=0.05),
-                    horizontalalignment='right', verticalalignment='top')
+        #detrended.plot(colors=['k','m'])
+        line1 = plt.plot_date(detrended.index, detrended[pair[0]].values, 
+                              linestyle='-', color='k', marker='None')
+        line2 = plt.plot_date(detrended.index, detrended[pair[1]].values, 
+                              linestyle='-', color='m', marker='None')
         
-       
+        # add legend with two extra bits of info in it...
+        handles = [line1[0],line2[0]]
+        labels = [pair[0], pair[1]]
+        # add handle,label with correlations in it
+        handles.append(patches.Patch(color='white'))
+        labels.append('r = %.3f'%corr.values[0,1])
+        handles.append(patches.Patch(color='white'))
+        labels.append('dt-r = %.3f'%detrended_corr.values[0,1])
+        plt.legend(handles,labels,loc='best')
+        
+        # correlations before and after detrending
+        #plt.annotate("trendy    r = %.3f"%corr.values[0,1], 
+        #             xy=(.085,.75), xycoords='axes fraction')
+        #plt.annotate("detrended r = %.3f"%detrended_corr.values[0,1], 
+        #             xy=(.085,.7), xycoords='axes fraction')
+        
+        if j < 4:
+            # remove xticks etc
+            plt.xticks([],[])
+            plt.xlabel('')
+            plt.gca().axes.xaxis.set_visible(False)
+    plt.suptitle('Detrended comparable metrics',fontsize=20)
+    plt.subplots_adjust(top=.95,hspace=0.02)
+    # rotate and align the tick labels so they look better
+    fig.autofmt_xdate()
+    fio.save_fig(model_run=model_run, script_name=_sn_, 
+                 pname='%s_%s_correlation%s'%(AWS,model_run,['','_UTC'][UTC]), plt=plt)
     
     return df_agg, subplots
     
@@ -283,8 +329,7 @@ if __name__=='__main__':
         for UTC in [True, False]:
             compare_site_to_model(AWS='wagerup',
                                   model_run=mr,
-                                  showrange=[datetime(2016,1,5,6),datetime(2016,1,7,1)],
+                                  showrange=[datetime(2016,1,5,6),datetime(2016,1,7,6)],
                                   UTC=UTC)
 
-#(df1,sp1), (dfold,spold) = df
 
