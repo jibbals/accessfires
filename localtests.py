218a75b73552e7030faf4a8bead4920c17f226d0
jibbals <jwg366@uowmail.edu.au>
2020-08-06 18:47:07 +1000

    reading functions for HC06D in localtests

--- localtests.py
+++ localtests.py
@@ -29,130 +29,126 @@ from utilities import utils, plotting, fio, constants
 
 from scipy import interpolate
 
-fdtime = datetime(2016,1,6,12)
-fdtime2 = datetime(2016,1,7,4)
-mr='waroona_run3'
 
-#cubes=fio.read_model_run(mr,[fdtime,fdtime2], add_z=True,add_winds=True)
-#print (cubes)
-#z, w, u, topog = cubes.extract(['z_th','upward_air_velocity','u','surface_altitude'])
-#lats,lons = w.coord('latitude').points, w.coord('longitude').points
-#print(z.shape)
+import pandas
 
-# Read firefront
-extent = plotting._extents_['waroonaf'] # synoptic extent
-ff, = fio.read_fire(model_run=mr, dtimes=None,
-                    extent=extent, firefront=True,
-                    HSkip=5)
+def read_HC06D_path(path):
+    """
+    Return Pandas table with Columns based on following 
+        original names:table names
+    {   'Day/Month/Year Hour24:Minutes in DD/MM/YYYY HH24:MI format in Local Time':'LocalTime',
+        'Air temperature in Degrees C':'Ta',
+        'Quality of air temperature':'Q_Ta',
+        'Dew point temperature in Degrees C':'Td',
+        'Quality of dew point temperature':'Q_Td',
+        'Wet bulb temperature in Degrees C':'Twb',
+        'Quality of wet bulb temperature':'Q_Twb',
+        'Relative humidity in percentage %':'RH',
+        'Quality of relative humidity':'Q_RH',
+        'Wind speed measured in m/s':'ws',
+        'Quality of wind speed':'Q_ws',
+        'Wind direction measured in degrees':'wd',
+        'Quality of wind direction':'Q_wd',
+        'Mean sea level pressure in hPa':'Pmsl',
+        'Quality of mean sea level pressure':'Q_Pmsl',
+        'Station level pressure in hPa':'Pstn',
+        'Quality of station level pressure':'Q_Pstn',
+        'QNH pressure in hPa':'Pqnh',
+        'Quality of QNH pressure':'Q_Pqnh',
+        'Vapour pressure in hPa':'Pv',
+        'Quality of vapour pressure':'Q_Pv',
+        'Saturated vapour pressure in hPa':'Psv',
+        'Quality of saturated vapour pressure':'Q_Psv',
+        'Total cloud amount in eighths':'Cloud',
+        'Quality of total cloud amount':'Q_Cloud',
+    }
+    """
+    path='data/AWS/HC06D_Data_061287_48628999806477.txt'
+    ## Read data
+    data = pandas.read_csv(path) # skiprows=1 removes titles
 
-
-from shapely import geometry
-#import matplotlib.pyplot as plt
-#from matplotlib import cm
-#import numpy as np
-import fiona
-import os,json
-#from descartes.patch import PolygonPatch
-
-# create some test data with multiple peaks
-lon = np.linspace(0,45,100)
-lat = np.linspace(-20,32,90)
-long,latg=np.meshgrid(lon,lat)
-C1=np.sqrt((long-5.)**2+(latg-25)**2)/30.
-C2=np.sqrt((long-30.)**2+(latg-1)**2)/10.
-m = 30*np.exp(-C1**2)+20.*np.exp(-C2**2)
-
-# make the contourf plot, storing the resulting ContourSet in cs
-plt.figure(figsize=[10,5])
-plt.subplot(1,2,1)
-Nlevels=10
-cs = plt.contourf(lon,lat,m,Nlevels,cmap='gist_heat')
-plt.title('contourf figure with Nlevels='+str(Nlevels))
-
-# create lookup table for levels
-lvl_lookup = dict(zip(cs.collections, cs.levels))
-
-# loop over collections (and polygons in each collection), store in list for fiona
-PolyList=[]
-for col in cs.collections:
-    z=lvl_lookup[col] # the value of this level
-    for contour_path in col.get_paths():
-        # create the polygon for this level
-        for ncp,cp in enumerate(contour_path.to_polygons()):
-            lons = cp[:,0]
-            lats = cp[:,1]
-            new_shape = geometry.Polygon([(i[0], i[1]) for i in zip(lons,lats)])            
-            if ncp == 0:
-                poly = new_shape # first shape
-            else:
-                poly = poly.difference(new_shape) # Remove the holes
-        PolyList.append({'poly':poly,'props':{'z': z}})
-
-## write the fiona collection
-
-# clean up directories
-outname=os.path.join('..','data','shaped_contour')
-if os.path.isdir(outname):
-    for file in os.listdir(outname):
-        os.remove(os.path.join(outname,file))
-    os.rmdir(outname)
-os.mkdir(outname)
-
-# define ESRI schema, write each polygon to the file
-outfi=os.path.join(outname,'shaped_contour.shp')
-schema = {'geometry': 'Polygon','properties': {'z': 'float'}}
-with fiona.collection(outfi, "w", "ESRI Shapefile", schema) as output:
-    for p in PolyList:
-        output.write({'properties': p['props'],
-            'geometry': geometry.mapping(p['poly'])})
-
-# save the levels and global min/max as a separate json for convenience
-Lvls={'levels':cs.levels.tolist(),'min':m.min(),'max':m.max()}
-with open(os.path.join(outname,'levels.json'), 'w') as fp:
-    json.dump(Lvls, fp)
-
-## Plotting the results: reads data back in, plots the polygons with data only
-## from shapefile and levels.txt
-
-ax=plt.subplot(1,2,2)
-
-# read in levels, define colormap
-with open(os.path.join(outname,'levels.json')) as jfile:
-    Lvls=json.load(jfile)
-levels=np.array(Lvls['levels'])
-cmap=plt.cm.gist_heat
-lv_range=[Lvls['min'],Lvls['max']]
-
-## loop over each shape, pull out level value ('z'), plot a polygon with a color
-## matching the colormap.
-#ishp=0
-#with fiona.open(outfi) as shape:
-#    for shp in shape:
-#
-#        # pull this shape's level and set the color from the map
-#        lv=shp['properties']['z'] # this shape's level
-#        clr=cmap((lv - lv_range[0])/(lv_range[1]-lv_range[0]))
-#
-#        # build the polygon and add the patch
-#        coords=shp['geometry']['coordinates'][0] # coords of this polygon
-#        poly=geometry.Polygon(coords)
-#        patch = PolygonPatch(poly, facecolor=clr, edgecolor=clr)
-#        ax.add_patch(patch)
-#
-#        # track max/min coordinate values
-#        bnds=poly.bounds
-#        rng_C={'lon':{'min':bnds[0],'max':bnds[2]},
-#               'lat':{'min':bnds[1],'max':bnds[3]}}
-#        if ishp==0:
-#            rngs=rng_C
-#        else:
-#            for ll in ['lon','lat']:
-#                rngs[ll]['max']=max([rngs[ll]['max'],rng_C[ll]['max']])
-#                rngs[ll]['min']=min([rngs[ll]['min'],rng_C[ll]['min']])
-#        ishp=ishp+1
-#
-#ax.set_xlim([rngs['lon']['min'],rngs['lon']['max']])
-#ax.set_ylim([rngs['lat']['min'],rngs['lat']['max']])
-#plt.title('polygon patches from shapefile')
-
-#plt.show()
+    # read datetimes
+    dind_str='Day/Month/Year Hour24:Minutes in DD/MM/YYYY HH24:MI format in Local Time'
+    # 01/01/2017 00:00
+    # convert column to datetime64 in the dataframe
+    data[dind_str] = pandas.to_datetime(data[dind_str], dayfirst=True)
+    
+    HC06D_dict = {
+        'Station Name':'Station Name',
+        'Locality':'Locality',
+        'Latitude':'Latitude',
+        'Longitude':'Longitude',
+        'Height above MSL':'Height above MSL',
+        'Day/Month/Year Hour24:Minutes in DD/MM/YYYY HH24:MI format in Local Time':'LocalTime',
+        'Air temperature in Degrees C':'Ta',
+        'Quality of air temperature':'Q_Ta',
+        'Dew point temperature in Degrees C':'Td',
+        'Quality of dew point temperature':'Q_Td',
+        'Wet bulb temperature in Degrees C':'Twb',
+        'Quality of wet bulb temperature':'Q_Twb',
+        'Relative humidity in percentage %':'RH',
+        'Quality of relative humidity':'Q_RH',
+        'Wind speed measured in m/s':'ws',
+        'Quality of wind speed':'Q_ws',
+        'Wind direction measured in degrees':'wd',
+        'Quality of wind direction':'Q_wd',
+        'Mean sea level pressure in hPa':'Pmsl',
+        'Quality of mean sea level pressure':'Q_Pmsl',
+        'Station level pressure in hPa':'Pstn',
+        'Quality of station level pressure':'Q_Pstn',
+        'QNH pressure in hPa':'Pqnh',
+        'Quality of QNH pressure':'Q_Pqnh',
+        'Vapour pressure in hPa':'Pv',
+        'Quality of vapour pressure':'Q_Pv',
+        'Saturated vapour pressure in hPa':'Psv',
+        'Quality of saturated vapour pressure':'Q_Psv',
+        'Total cloud amount in eighths':'Cloud',
+        'Quality of total cloud amount':'Q_Cloud',
+    }
+    
+    # rename using dict
+    data = data.rename(columns=HC06D_dict)
+    # set local time to be the index
+    data = data.set_index('LocalTime')
+    return data.loc[:,HC06D_dict.values()]
+    
+def read_HC06D(sites=[], extent=None):
+    """
+    READ AVAILABLE HC06D sites
+    ARGUMENTS:
+        subset to sites list (optional)
+        subset to extent [left,right,bot,top] in degrees (optional)
+    """
+    site_numlatlon={
+        'COONABARABRAN':['064008',-31.2786,149.2786],
+        'COONABARABRAN AIRPORT':['064017',-31.3330,149.2699],
+        'DUNEDOO':['064009',-32.0165,149.3956],
+        'GULGONG':['062013',-32.3634,149.5329],
+        'MERRIWA':['061287',-32.19,150.17],
+        'MUDGEE':['062101',-32.5628,149.6149],
+        'NULLO MOUNTAIN':['062100',-32.7244,150.2290],
+        'WELLINGTON':['065034',-32.5635,148.9503],
+        }
+    #site_numlatlon['NULLO MOUNTAIN']=site_numlatlon['NULLO'] # alternate name
+    
+    
+    ret_data={} # dataframes to be returned
+    if len(sites) > 0:
+        # subset by site names
+        for site in sites:
+            path='HC06D_Data_%s_48628999806477.txt'%(site_numlatlon[site][0])
+            ret_data[site]=read_HC06D_path(path)
+    elif extent is not None:
+        # subset by extent
+        for site in site_numlatlon.keys():
+            num,lat,lon = site_numlatlon[site]
+            if (lat>extent[2]) and (lat<extent[3]) and (lon>extent[0]) and (lon<extent[1]):
+                path='HC06D_Data_%s_48628999806477.txt'%(num)
+                ret_data[site]=read_HC06D_path(path)
+    else:
+        # grab all
+        for site in site_numlatlon.keys():
+            path='HC06D_Data_%s_48628999806477.txt'%(site_numlatlon[site][0])
+            ret_data[site]=read_HC06D_path(path)
+    return ret_data
+    #dates = [datetime.strptime(dstr, "%d/%m/%Y %H:%M") for dstr in data.loc[:,dind_str]]
